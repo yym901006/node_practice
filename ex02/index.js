e8e0150d847021dad07d5a0d9aa7058b281d5eca
yangyongming <yangyongming@kuaidihelp.com>
2020-07-08 23:06:07 +0800

    @yym：

--- ex02/index.js
+++ ex02/index.js
@@ -1,10 +1,18 @@
-module.exports.compose = middlewares => {
-    return function () {
-        return dispatch(0)
-        function dispatch(i) {
-            // ##BEGIN## 代码已加密
-            
-            // ##END##
-        }
+module.exports.compose = (middlewares) => {
+  return function () {
+    return dispatch(0);
+    function dispatch(i) {
+      // ##BEGIN## 代码已加密
+      const fun = middlewares[i];
+      if (!fun) {
+        return Promise.resolve();
+      }
+      return Promise.resolve(
+        fun(function next() {
+          dispatch(i + 1);
+        })
+      );
+      // ##END##
     }
-}
+  };
+};
