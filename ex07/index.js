878cdaed41066f19edd97040d9448db2679ebaba
yangyongming <yym901006@163.com>
2020-07-18 19:27:54 +0800

    @yym：

--- ex07/index.js
+++ ex07/index.js
@@ -1,25 +1,23 @@
-const fs = require('fs')
-module.exports.createLoader = config => {
-    const loader = (scanFolder, cb) => {
-        const files = fs.readdirSync(scanFolder);
-        files.forEach(filename => {
-            filename = filename.replace(".js", "");
-            const file = require(scanFolder + "/" + filename);
-            cb(filename, file);
-        })
-    }
+const fs = require("fs");
+module.exports.createLoader = (config) => {
+  const loader = (scanFolder, cb) => {
+    const files = fs.readdirSync(scanFolder);
+    files.forEach((filename) => {
+      filename = filename.replace(".js", "");
+      const file = require(scanFolder + "/" + filename);
+      cb(filename, file(config));
+    });
+  };
 
-    return {
-        initFunction: scanFolder => {
-            const ret = {}
-            // ##BEGIN## 代码已加密
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEJOEEOOIOSOOSSOEXJPPOEIOSJOOIOESJIIOEEOEJOSOOSSOEXJPAJEHJPPOSEOSXOEJOSSOESOOIOEOOSSJPAJEHOSJOEXOSSOOIOEAOSSJIIOPJOESJPHJEHJXIJXAJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOSJOEEOESOEIOEAJEHOSEOPJOESOSJJEHJXIJEHOSJOEXOSSOOIOEAOSSJIIOPJOESJPPOSJOEEOESOSEOSXOSPJPH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEAOOOOSEOSXOEJOSSOESOOIOEOOSSOOEJEHJXIJEHOSEOPJOESOSJ
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOPAJPH
-            // ##END##
-            return ret
-        }
-    }
-}
-
+  return {
+    initFunction: (scanFolder) => {
+      const ret = {};
+      // ##BEGIN## 代码已加密
+      loader(scanFolder, (filename, file) => {
+        ret[filename] = file;
+      });
+      // ##END##
+      return ret;
+    },
+  };
+};
